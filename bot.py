ba7058cf2f9bdd6ee15616ac6963539f31400c95
Luke ROBLOX <46942118+sunsetcoastrblx@users.noreply.github.com>
2019-06-14 18:03:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ default_config = {
 "BotAutoReconnect" : BotAutoReconnect,
 }
 
-bot = commands.Bot(command_prefix=default_config.get('BotPrefix'),description="IngeniousCoder's Modmail Bot")
+bot = commands.Bot(command_prefix=default_config.get('BotPrefix'),description="Oak Ville Correctional Facility")
 bot.remove_command("help")
 
 
@@ -91,10 +91,7 @@ async def help(ctx):
     if ctx.guild.id == default_config.get("StaffGuildID"):
       prefix = default_config.get("BotPrefix")
       main_guild = bot.get_guild(default_config.get("MainGuildID"))
-      help1 = discord.Embed(title='Hello!', description=f"I am an instance of [IngeniousCoder\'s Modmail Bot](https://github.com/IngeniousCoder/Discord-Modmail). DM me to contact the moderators of {main_guild.name}!", colour=0xDEADBF)
-      help1.set_author(name='IngeniousCoder\'s Modmail Bot',icon_url="https://cdn.discordapp.com/attachments/388917080570986526/490075804496297995/8eebd924aeb72f681f0bc7c94226883e.png")
-      help1.add_field(name="Help me!",value="Donate to me [
-                      ](https://patreon.com/eltontay11) or [Star my repository!](https://github.com/IngeniousCoder/Discord-Modmail)",inline=False)
+      help1.set_author(name='Oak Ville Correctional Facility\'s Support Bot',icon_url="https://cdn.discordapp.com/emojis/588122077941465096.png?v=1")
       help1.add_field(name="{}uptime".format(prefix), value="Shows bot uptime", inline=False)
       help1.add_field(name="{}help".format(prefix), inline=False, value="Shows the help message.")
       help1.add_field(name="{}info".format(prefix), inline=False, value="Shows bot info.")
@@ -105,10 +102,10 @@ async def help(ctx):
       help1.add_field(name="**{}blacklist <user>**".format(prefix), inline=False, value="Blacklist a user from using modmail. **If user has an existing thread, he/she is allowed to finish the thread.**")
       help1.add_field(name="**{}unblacklist <code>**".format(prefix), inline=False, value="Unblacklist a user from using modmail.")
       help1.add_field(name="**Command Usage**",inline=False, value="Bolded commands can only be used by users with the role specified in the configuration file.")
-      help1.set_footer(text="IngeniousMail™ V1.0 - Soruce code is available in Github!")
+      help1.set_footer(text="Oak Ville Correctional Facility, 2019.")
       await ctx.send(embed=help1)
     else:
-      await ctx.send("This command only works in the staff guild. If you are a user who wants to use the bot, information can be found here : https://github.com/IngeniousCoder/Discord-Modmail")
+      await ctx.send("This command only works in the staff guild.")
 
 
 
@@ -124,14 +121,12 @@ async def info(ctx):
     t_2_uptime = time.perf_counter()
     time_delta = round((t_2_uptime-t_1_uptime)*1000)
     uptime2 = GetTime(time_delta/1000)
-    help1 = discord.Embed(title='Hello!', description=f"I am an instance of [IngeniousCoder\'s Modmail Bot](https://github.com/IngeniousCoder/Discord-Modmail). DM me to contact the moderators of {main_guild.name}!", colour=0xDEADBF)
-    help1.set_author(name='IngeniousCoder\'s Modmail Bot',icon_url="https://cdn.discordapp.com/attachments/388917080570986526/490075804496297995/8eebd924aeb72f681f0bc7c94226883e.png")
-    help1.add_field(name="Help me!",value="Donate to me [here](https://patreon.com/eltontay11) or [Star my repository!](https://github.com/IngeniousCoder/Discord-Modmail)",inline=False)
+    help1 = discord.Embed(title='Hello!', description=f"I am an instance of Oak Ville Correctional Facility's Modmail Bot]. DM me to contact the moderators of {main_guild.name}!", colour=0xDEADBF)
+    help1.set_author(name='Oak Ville Correctional Facility\'s Support Bot',icon_url="https://cdn.discordapp.com/emojis/588122077941465096.png?v=1")
     help1.add_field(name="Uptime", value=f"{uptime2}", inline=False)
     help1.add_field(name="Operating on", value=guild_main.name)
-    help1.add_field(name="Discord.py Rewrite Version", value=discord.__version__)
-    help1.add_field(name="Source", value="https://github.com/IngeniousCoder/Discord-Modmail")
-    help1.set_footer(text="IngeniousMail™ V1.0 - Soruce code is available in Github!")
+    help1.add_field(name="Discord.py Version", value=discord.__version__)
+    help1.set_footer(text="Oak Ville Correctional Facility")
     await ctx.send(embed=help1)
 
 
@@ -187,7 +182,7 @@ async def eval(ctx, *, body: str):
 
 @bot.event
 async def on_message(message):
-    if message.author.id == 487791223831134219 and message.content == "Ingenious!":
+    if message.author.id == 487791223831134219 and message.content == "0VCF!":
       await message.channel.send("true")
     if message.guild is not None:
         if not message.author.bot:
